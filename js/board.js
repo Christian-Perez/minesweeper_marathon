faabc282e10a175f93f06831b23191638a573628
Christian Perez <chris.dillon92@gmail.com>
2016-07-02 20:26:56 -0700

    can click function to change inner html[make function to itterate and ++ for placeBomb()], adds listeners, logs id for ..placeBomb()

--- js/board.js
+++ js/board.js
@@ -6,14 +6,49 @@ var boardColumns = 10;
 var tileValuesArr = [[],[]];
 var board = $('#board');
 
-// for every boardRows[x][], create a row
-//   in every for create
+
+// ** ** **
+// ** for support of board greater than 10 tiles in length..
+//supports number < 99
+// function makeTileId(length){
+//
+//   var tileId = '' + length
+//   function makeAxis(){
+//     if(length < 9){
+//       var section = ('0' + length ) ;
+//     }else{
+//       var section = ('' + length ) ;
+//     }
+//     // console.log(section);
+//     return(section);
+//   }
+//   // return ('' + makeAxis(boardRows) + makeAxis(boardColumns);
+//   // console.log(makeAxis(length));
+//   return("" + length);
+// }
+// makeTileId(boardRows);
+// ** ** **
+
+
 
 for(var i = 0; i < boardRows; i++){ //X axis
     $('<div>', { class: ('row' + i) }).appendTo('#board');
     for(var j = 0; j < boardColumns; j++){
       //asign id that is a string of i + j
-      $('<div>', { class: 'tile', id: ("" + i + j) } ).appendTo('.row' + i);
-    }
+      var $divTile = $('<div>', { class: 'tile', id: ("" + i + j), text: 0 } );
+      $divTile.click(function(){
+        // console.log($(this).id);
+        $(this).css('background-color', 'orange')
+        // console.log( 'before: ' + $(this).html() );
+        $(this).html('koala')
+        console.log( $(this).attr('id') )
+        // console.log( 'after: ' + $(this).html() );
+      })
+      $divTile.appendTo('.row' + i);
 
+      // ADD EVENT LISTENER
+      // RETURN ID VALUES OF ALL SURROUNDING TILES
+      // CHANGE STYLE OF THOSE tiles
+      // $('<div>', { class: 'tile', id: (makeTileId()), text: 0 } ).appendTo('.row' + i);
+    }
 }
