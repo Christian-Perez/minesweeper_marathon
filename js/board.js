de6703dfd7eda1faef12d3e5137978a061d68c2a
Christian Perez <chris.dillon92@gmail.com>
2016-07-05 12:05:25 -0700

    WORKING! make exact num of bombs

--- js/board.js
+++ js/board.js
@@ -2,7 +2,13 @@
 var board = $('#board');
 var boardRows = 10;
 var boardColumns = 10;
-var bombsNum = 10;
+var numOfBombs = 10;
+
+function randomTileAxisNum(){
+  // * has to adapt to different board sizes
+  var num = Math.floor( (Math.random() * 10 ) )
+  return num
+}//randomNum()
 
 function makeTileIdStr(row, col){ // max board size 100x
   function makeAxisStr(num){
@@ -11,35 +17,43 @@ function makeTileIdStr(row, col){ // max board size 100x
     }else{
       return ( '' + num )
     }
-  } // makeAxisStr
+  } // makeAxisStr(num)
   return makeAxisStr(row) + makeAxisStr(col)
-} // makeTileIdStr
+} // makeTileIdStr(row, col)
 
 function makeIntoBomb(tileIdStr){
+  /// ? refactor to accept str or num ?
   $('#' + tileIdStr).css('background-color', 'orange')
   $('#' + tileIdStr).html('-1')
 
   var bombRowNum = parseInt(tileIdStr.slice(0, 2))
   var bombColNum = parseInt(tileIdStr.slice(2))
   /// UPDATE SURROUNDING TILES
-  // define path
+  // define path directions
   var shift = {
+    // tested
     up: [-1, 0],
     down: [1, 0],
     left: [0, -1],
-    right: [0, 1]
+    right: [0, 1],
+    // not tested ..
+    ul: [-1, -1],
+    ur: [-1, 1],
+    dl: [1, -1],
+    dr: [1, 1]
   }
+  // define path to targets
   var path = ['up', 'left', 'down', 'down', 'right', 'right', 'up', 'up']
   // start path at the bomb
-    var targetColNum = bombColNum;
-    var targetRowNum = bombRowNum;
-    // walk the path
-    for(var t = 0; t < path.length; t++){
-      var direction = path[t]
-          targetRowNum += shift[direction][0]
-          targetColNum += shift[direction][1]
-      var targetId = makeTileIdStr(targetRowNum, targetColNum)
-      var $targetTile = $('#' + targetId);
+  var targetColNum = bombColNum;
+  var targetRowNum = bombRowNum;
+  // walk the path
+  for(var t = 0; t < path.length; t++){
+    var direction = path[t]
+        targetRowNum += shift[direction][0]
+        targetColNum += shift[direction][1]
+    var targetId = makeTileIdStr(targetRowNum, targetColNum)
+    var $targetTile = $('#' + targetId);
 
       if($targetTile.html() == '-1'){
         console.log(targetId)
@@ -48,27 +62,42 @@ function makeIntoBomb(tileIdStr){
         $targetTile.html(parseInt($targetTile.html()) + 1)
       }
     } // for( pathThroughTargets )
-  } // makeIntoBomb()
+  } // MAKE INTO BOMB ()
+
+function isBomb(){
+
+}
 
 // function makeBoardDOM(){
+// MAKE BOARD
 for(var row = 0; row < boardRows; row++){ // ROW
   $('<div>', { id: ('row' + row), class: 'row' }).appendTo('#board');
+
   for(var col = 0; col < boardColumns; col++){ // COLUMN
     var $divTile = $('<div>', { class: 'tile', id: makeTileIdStr(row, col), text: 0 } );
+      $divTile.click(function(){
+        console.log( 'clicked: ' + $(this).attr('id') + ', isBomb: ' + '? isBomb() ?' )
+        // makeIntoBomb( $(this).attr('id') )
+      }) // $divTile.click
+      $divTile.appendTo('#row' + row);
+  } // forEach( column )
+} // forEach( row )
 
-    $divTile.click(function(){
-      makeIntoBomb( $(this).attr('id') )
-      // console.log( $(this).attr('id') )
-    }) // divTile.click
+/// MAKE ARRAY OF UNIQUE BOMB IDs
+var newBombId = makeTileIdStr( randomTileAxisNum(), randomTileAxisNum() );
+var arrayOfBombs = [newBombId]
+while(arrayOfBombs.length < numOfBombs){
+  newBombId = makeTileIdStr( randomTileAxisNum(), randomTileAxisNum() );
+  console.log('newBombId: ' + newBombId)
+  if( !arrayOfBombs.includes(newBombId) ){
+    arrayOfBombs.push(newBombId)
+    console.log('pushed ' + newBombId)
+  }
+  console.log('arrayOfBombs: ' + arrayOfBombs.length)
+} // while(arrayOfBombs.length < numOfBombs)
 
-    $divTile.appendTo('#row' + row);
-
-    /// lay down bomb? if this id = bombid..
-    //var randomNum = Math.floor( (Math.random() * 10 ) )
-
-    // console.log(randomNum)
-
-  } // COLUMN
-} // ROW
-
-// }makeBoardDOM()
+/// UTILIZE BOMBS ARR TO MAKE BOMBS
+for(var b = 0; b < arrayOfBombs.length; b++){
+  makeIntoBomb(arrayOfBombs[b])
+}
+// } makeBoardDOM()
