2c4fe455a17e1f8361ceb56921e8f36eb6e1986a
Christian Perez <chris.dillon92@gmail.com>
2016-07-06 16:41:44 -0700

    working, working extract of traverseBoard, before refactor of makeIntoBomb

--- js/game.js
+++ js/game.js
@@ -1,2 +1,19 @@
+function traverseTiles(tileIdStr, directionStr){
+  // define path directions
+  var shift = {
+    up: [-1, 0],
+    down: [1, 0],
+    left: [0, -1],
+    right: [0, 1],
+  }
 
-// function traverseToTile(startTile, direction){}
+  // start at titleIdStr
+  var targetRowNum = parseInt(tileIdStr.slice(0, 2))
+  var targetColNum = parseInt(tileIdStr.slice(2))
+  //shift tile focus
+  targetRowNum += shift[directionStr][0]
+  targetColNum += shift[directionStr][1]
+
+  return makeTileIdStr(targetRowNum, targetColNum)
+
+} // traverseTile(tileIdStr, directionStr)
